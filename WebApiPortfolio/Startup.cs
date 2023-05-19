9ce352b65753266b9bfd5fd6dc3640a344c04502
electr0nauta <rodrigo.martin1790@gmail.com>
2023-05-19 20:27:56 -0300

    primer commit

---
+++ WebApiPortfolio/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApiPortfolio
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+
+        }
+    }
+}
